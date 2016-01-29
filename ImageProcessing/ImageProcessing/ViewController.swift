3207a9a389745485c3bcf86fd21f28a0f62b16ff
sunnyjoe <jiaoqingsunny@gmail.com>
2016-01-29 14:49:29 +0800

    no message

---
+++ ImageProcessing/ImageProcessing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageProcessing
+//
+//  Created by jiao qing on 29/1/16.
+//  Copyright © 2016 jiao qing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
